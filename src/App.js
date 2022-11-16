bf6a979a6f533b3db72794cca1da31f6ec65147b
stevendrawings <stevenchauvets@gmail.com>
2022-11-16 13:22:56 +0100

    Ajout du prenom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Membre from './components/Membre'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className='App'>
+<h1>React App stevens</h1>
+<Membre />
+    </header>
+
   );
 }
 
